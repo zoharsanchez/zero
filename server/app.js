3a9a75e8c9220075e871ec7e2bf164f36de5732b
juliusbuckley <juliusbuckley@gmail.com>
2016-09-08 00:52:58 -0700

    chores/ Updated app, db, and deleted entry.js.

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import path from 'path';
-import routes from './routes/entry';
+import slackRoutes from './routes/slackRoutes';
 
 const app = express();
 const port = process.env.PORT || 8020;
 
 app.use(express.static(path.join(__dirname,'../client')));
 
-routes(app);
+slackRoutes(app);
 
 app.listen(port,() => {
   console.log(`app.js has been served on port: ${port}`);
